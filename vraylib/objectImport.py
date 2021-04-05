d787d398658f7c84500dff456d55ccd89958a7d1
FaruNuriSonmez <info@farunurisonmez.com>
2021-04-05 15:03:06 +0300

    PyCharm CE
    ...
    
    Co-Authored-By: Faru Nuri Sönmez <info@farunurisonmez.com>

--- vraylib/objectImport.py
+++ vraylib/objectImport.py
@@ -16,7 +16,6 @@ def make_transform(rotX=0, rotY=0, rotZ=0, scale=1, offset=vray.Vector()):
     transform = vray.Transform(mS * mZ * mY * mX, offset)
     return transform
 
-
 def setup_scene(renderer, obj_diameter):
     renderView = renderer.classes.RenderView()
 
@@ -37,15 +36,15 @@ def setup_scene(renderer, obj_diameter):
     rect.intensity = 10
 
 
-def to_vector_list (vertc):
-    print(vertc)
+def to_vector_list(vertc):
+    #print(vertc)
     vrayVectorList = vray.VectorList()
-    i=0
+    i = 0
     while i < len(vertc):
         vrayVector = vray.Vector(vertc[i], vertc[i+1], vertc[i+2])
         i += 3
         vrayVectorList.append(vrayVector)
-    print(vrayVectorList)
+    #print(vrayVectorList)
     return vrayVectorList
 
 def to_int_list(vrayIList):
@@ -55,7 +54,16 @@ def to_int_list(vrayIList):
     #print(vrayIntList)
     return vrayIntList
 
+def to_textures_vector_list(vectors):
+    #print(vectors)
+    vrayVectorList = vray.VectorList()
+    i = 0
+    while i < len(vectors):
+        vrayVector = vray.Vector(vectors[i], vectors[i + 1], 0)
+        i += 2
+        vrayVectorList.append(vrayVector)
 
+    return vrayVectorList
 
 def add_obj(renderer, material, smooth=False):
     if len(vertices) == 0 or len(faces) == 0:
@@ -68,9 +76,10 @@ def add_obj(renderer, material, smooth=False):
     #   mesh.normals = obj.vertexNormals
     #   mmesh.faceNormals = obj.faceNormals
 
-    #if len(obj.textureVertices) > 0 and len(obj.faceTextures) > 0:
-    #   channel = [1, obj.textureVertices, obj.faceTextures]
-    #   mesh.map_channels = [channel]
+    if len(texturesVertices) > 0 and len(texturesFaces) > 0:
+        vrayTexturesVertices = to_textures_vector_list(texturesVertices)
+        channel = [1, vrayTexturesVertices, mesh.faces]
+        mesh.map_channels = [channel]
 
     if smooth:
         smoothed = renderer.classes.GeomStaticSmoothedMesh()
@@ -83,26 +92,45 @@ def add_obj(renderer, material, smooth=False):
     # adjust Y-up object to Z-up scene
     node.transform = make_transform(math.pi / 2, 0, 0)
 
-
 if __name__ == "__main__":
-    GLTFVRayLoader.gltf_loader(filename="../../glTF-Sample-Models/sugartech/tp_livelokonsol_High1433d.glb")
-    to_int_list(faces)
+    GLTFVRayLoader.gltf_loader(filepath="../../glTF-Sample-Models/sugartech/tp_livelokonsol_High1433.glb", texpath=".././assets/textures")
     with vray.VRayRenderer() as renderer:
+
         renderer.keepInteractiveRunning = True
 
+        bitmap = renderer.classes.BitmapBuffer()
+        bitmap.file = "/Users/farunurisonmez/Documents/GitHub/PythonExamples/assets/textures/3.png"
+
+        normalBitmap = renderer.classes.BitmapBuffer()
+        normalBitmap.file = "/Users/farunurisonmez/Documents/GitHub/PythonExamples/assets/textures/1.png"
+
+        texture = renderer.classes.TexBitmap()
+        texture.bitmap = bitmap
+
+        normalTexture = renderer.classes.TexBitmap()
+        normalTexture.bitmap = normalBitmap
+
         brdf = renderer.classes.BRDFVRayMtl()
-        brdf.diffuse = vray.AColor(0.5, 1.0, 0.5, 1.0)
+        brdf.diffuse = texture
+        brdf.bump_map = normalTexture
         brdf.reflect = vray.AColor(0.8, 0.8, 0.8, 1.0)
         brdf.fresnel = True
+
+        x = renderer.classes.BRDFAlSurface()
+        x.diffuse = texture
+        x.bump_map = normalTexture
+        x.bump_type = 6
+
+
         material = renderer.classes.MtlSingleBRDF()
-        material.brdf = brdf
+        material.brdf = texture
 
         parser = ObjParser(triangulator=ConvexTriangulator)
         add_obj(renderer, material)
 
         setup_scene(renderer, 3)
-
         # you can use the VFB mouse controls to orbit around the object
         # (shift + LMB drag)
+
         renderer.start()
         renderer.waitForRenderEnd()
