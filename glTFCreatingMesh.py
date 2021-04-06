4dcfde38bc60b5a3c82a15c0a4d3202078a60269
fatih erol <>
2021-04-06 13:01:47 +0300

    Vertices, Normals, Textures Added

--- glTFCreatingMesh.py
+++ glTFCreatingMesh.py
@@ -13,16 +13,39 @@ def gltfCreatingMesh(width, length):
         ],
         dtype="float32",
     )
-    triangles = np.array(
+
+    normals = np.array(
         [
-            [0, 1, 2],
-            [3, 2, 1],
+            [0, 1, 0],
+            [0, 1, 0],
+            [0, 1, 0],
+            [0, 1, 0],
+        ],
+        dtype="float32",
+    )
+
+    indices = np.array(
+        [
+            [2, 1, 0],
+            [1, 2, 3],
         ],
         dtype="uint8",
     )
 
-    triangles_binary_blob = triangles.flatten().tobytes()
-    points_binary_blob = vertices.tobytes()
+    texture_coordinates = np.array(
+        [
+            [0.0, 0.0],
+            [1.0, 0.0],
+            [0.0, 1.0],
+            [1.0, 1.0],
+        ],
+        dtype="float32"
+    )
+
+    indices_binary_blob = indices.flatten().tobytes()
+    vertices_binary_blob = vertices.tobytes()
+    texture_coordinates_binary_blob = texture_coordinates.tobytes()
+    normals_binary_blob = normals.tobytes()
 
     gltf = GLTF2(
         asset=[
@@ -48,9 +71,9 @@ def gltfCreatingMesh(width, length):
                     Primitive(
                         attributes=Attributes(
                             POSITION=1,
-                            #NORMAL=1,
-                            #TEXCOORD_0=2
-                            ),
+                            NORMAL=3,
+                            TEXCOORD_0=2
+                        ),
                         indices=0,
                         material=0
                     )
@@ -61,10 +84,10 @@ def gltfCreatingMesh(width, length):
             Accessor(
                 bufferView=0,
                 componentType=UNSIGNED_BYTE,
-                count=triangles.size,
+                count=indices.size,
                 type=SCALAR,
-                max=[int(triangles.max())],
-                min=[int(triangles.min())],
+                max=[int(indices.max())],
+                min=[int(indices.min())],
             ),
             Accessor(
                 bufferView=1,
@@ -74,44 +97,86 @@ def gltfCreatingMesh(width, length):
                 max=vertices.max(axis=0).tolist(),
                 min=vertices.min(axis=0).tolist(),
             ),
+            Accessor(
+                bufferView=2,
+                componentType=FLOAT,
+                count=len(texture_coordinates),
+                type=VEC2,
+                max=texture_coordinates.max(axis=0).tolist(),
+                min=texture_coordinates.min(axis=0).tolist(),
+            ),
+            Accessor(
+                bufferView=3,
+                componentType=FLOAT,
+                count=len(normals),
+                type=VEC3
+
+            ),
         ],
         bufferViews=[
             BufferView(
                 buffer=0,
-                byteLength=len(triangles_binary_blob),
+                byteLength=len(indices_binary_blob),
                 target=ELEMENT_ARRAY_BUFFER,
             ),
             BufferView(
                 buffer=0,
-                byteOffset=len(triangles_binary_blob),
-                byteLength=len(points_binary_blob),
+                byteOffset=len(indices_binary_blob),
+                byteLength=len(vertices_binary_blob),
+                target=ARRAY_BUFFER,
+            ),
+
+            BufferView(
+                buffer=0,
+                byteOffset=len(indices_binary_blob) +
+                len(vertices_binary_blob),
+                byteLength=len(texture_coordinates_binary_blob),
+                target=ARRAY_BUFFER,
+            ),
+
+            BufferView(
+                buffer=0,
+                byteOffset=len(indices_binary_blob) +
+                len(vertices_binary_blob) +
+                len(texture_coordinates_binary_blob),
+                byteLength=len(normals_binary_blob),
                 target=ARRAY_BUFFER,
             ),
         ],
         materials=[
             Material(
                 alphaMode=OPAQUE,
-                alphaCutoff=0.0,
                 doubleSided=True,
                 name="Material.001",
                 pbrMetallicRoughness=PbrMetallicRoughness(
-                    #baseColorTexture=0,
-                    baseColorFactor=[0.0, 0.0, 0.0, 0.0],
+                    baseColorTexture=TextureInfo(
+                        index=0,),
                 ),
             )
         ],
         textures=[
             Texture(
+                source=0,
+                name='name'
             )
         ],
+
         buffers=[
             Buffer(
-                byteLength=len(triangles_binary_blob) + len(points_binary_blob)
+                byteLength=len(indices_binary_blob) + len(vertices_binary_blob) +
+                len(texture_coordinates_binary_blob) + len(normals_binary_blob)
             )
         ],
     )
-    gltf.set_binary_blob(triangles_binary_blob + points_binary_blob)
+    image = Image()
+    image.uri = "resim.png"
+    image.name = "resimName"
+    gltf.images.append(image)
+    gltf.convert_images(ImageFormat.DATAURI)
+    gltf.set_binary_blob(indices_binary_blob + vertices_binary_blob +
+                         texture_coordinates_binary_blob + normals_binary_blob)
     gltf.save("plane.glb")
 
+
 if __name__ == "__main__":
-   gltfCreatingMesh(5, 5)
+    gltfCreatingMesh(5, 5)
